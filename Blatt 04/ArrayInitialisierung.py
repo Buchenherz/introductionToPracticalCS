db6441e0ef9a234898edb29849e3b6ece4ee8173
Clemens Pfister <pfistecl@me.com>
2016-11-15 21:54:11 +0100

    Renamed functions
    Shorter function name

--- Blatt 04/ArrayInitialisierung.py
+++ Blatt 04/ArrayInitialisierung.py
@@ -1,10 +1,10 @@
 import random
 
 # Variable initialisation
-z = int(input("Enter number Z (rng from 0 to Z): ")) # Whole number - will be used from 0 to Z
-n = int(input("Enter array length N: ")) # Array length - number of array items
+#z = int(input("Enter number Z (rng from 0 to Z): ")) # Whole number - will be used from 0 to Z
+#n = int(input("Enter array length N: ")) # Array length - number of array items
 
-def initialiseArray(z, n):
+def initArray(z, n):
 
     # Create array with size n
     a = [0] * n
@@ -14,9 +14,9 @@ def initialiseArray(z, n):
         #...and set a random number between 0 and z
         a[i] = random.randint(0,z)
     # So we see something
-    print(a)
+    # print(a)
     # Return
     return a
 
 # Function call
-initialiseArray(z, n)
+# initArray(z, n)
