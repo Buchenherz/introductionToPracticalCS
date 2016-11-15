0e89c217a03437e6c4b4226b43461f3ae30ad54a
Clemens Pfister <pfistecl@me.com>
2016-11-15 20:41:27 +0100

    Inital commit of sheet 04
    Added PyCharm Project and first exercise

---
+++ Blatt 04/ArrayInitialisierung.py
@@ -0,0 +1,22 @@
+import random
+
+# Variable initialisation
+z = int(input("Enter number Z (rng from 0 to Z): ")) # Whole number - will be used from 0 to Z
+n = int(input("Enter array length N: ")) # Array length - number of array items
+
+def initialiseArray(z, n):
+
+    # Create array with size n
+    a = [0] * n
+
+    # Go through every element in the array...
+    for i in range(0,n):
+        #...and set a random number between 0 and z
+        a[i] = random.randint(0,z)
+    # So we see something
+    print(a)
+    # Return
+    return a
+
+# Function call
+initialiseArray(z, n)
