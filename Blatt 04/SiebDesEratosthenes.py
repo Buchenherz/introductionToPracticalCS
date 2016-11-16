735fc88c15ca0fd464212f726b68453aa8df218c
Clemens Pfister <pfistecl@me.com>
2016-11-16 11:30:40 +0100

    Added algorithm for prime numbers

---
+++ Blatt 04/SiebDesEratosthenes.py
@@ -0,0 +1,20 @@
+limit = 1000 #int(input("Print prime numbers to: ")) # Whole number
+
+flags = {}
+for i in range(2, limit + 1):
+    flags[i] = True
+
+def getPrimes(limit):
+
+    for p in range(2, limit+1):
+        if flags[p] is True:
+            print(p)
+            m = p*p
+            while m <= limit:
+                flags[m] = False
+                m += p
+
+getPrimes(limit)
+#while limit <= 1000000:
+#    getPrimes(limit)
+#    limit *= 10
